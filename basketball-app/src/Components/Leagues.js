7c22036ab26cb74c240e591f4b18cc84680a54a1
Glenn Nguyo <glennnguyo1000@gmail.com>
2022-07-27 11:30:00 +0300

    get

--- basketball-app/src/Components/Leagues.js
+++ basketball-app/src/Components/Leagues.js
@@ -1,6 +1,18 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "../App";
 function Leagues(){
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        axios
+          .get("https://api-basketball.p.rapidapi.com/leagues")
+          .then((response) => {
+            console.log(response.data.data);
+            setData(response.data.data);
+          })
+          .catch((err) => console.log(err));
+      }, []);
+
     return(
         <div className="leagues">
 
