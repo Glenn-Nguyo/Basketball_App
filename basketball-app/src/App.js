bb21be4dd8486541ab42c351b60ab54c2a1e1f50
Glenn Nguyo <glennnguyo1000@gmail.com>
2022-07-27 11:17:15 +0300

    updated App

--- basketball-app/src/App.js
+++ basketball-app/src/App.js
@@ -1,8 +1,13 @@
 import './App.css';
-
+import Navbar from './Components/Navbar';
+import Content from './Components/Content';
+import Footer from './Components/Footer';
 function App() {
   return (
     <div className="App">
+        <Navbar />
+      <Content />
+      <Footer />
     </div>
   );
 }
